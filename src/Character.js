cb19cf13f01c2456fd214b4e46dbd90f71e6175b
andrescasa-dev <andrescasa.dev@gmail.com>
2022-06-14 16:58:51 -0500

    take damage logic was made

--- src/Character.js
+++ src/Character.js
@@ -1,32 +1,58 @@
 export default class Character {
   constructor(data){
     Object.assign(this, data);
+    this._diceArray = [];
+    this._enemy;
   }
 
   getDiceRolledArray(){
     return new Array(this.diceCount).fill(undefined).map((p, i, arr) => arr[i] = Math.floor(Math.random() * 10 + 1));
   }
-  // execute first time displayed
-  getDicePlaceholderHtml(){
-    return new Array(this.diceCount).fill(`<div class="placeholder-dice"></div>`).reduce((innerHtml, div) => innerHtml + div ,'');
-  }
-  
-  getDiceHTML(){
-    return this.getDiceRolledArray().reduce((acc, dice) => acc + `<div class="dice">${dice}</div>`, '');
+
+  getDiceContent(){
+    let diceContent ='';
+    if(this._diceArray.length !== 0)
+      diceContent = this._diceArray
+        .reduce((acc, dice) => acc + `<div class="dice">${dice}</div>`, '')
+    else
+       diceContent = new Array(this.diceCount)
+        .fill(`<div class="placeholder-dice"></div>`)
+        .reduce((innerHtml, div) => innerHtml + div ,'');
+    return diceContent;       
   }
 
+  diceRoll(){
+    this._diceArray = this.getDiceRolledArray();
+    return ;
+  }
+  // this method could be wrong 
   getInnerHtml(diceRolled = false){
-    const diceContent = diceRolled ? this.getDiceHTML() : this.getDicePlaceholderHtml();
     return`
     <div class="character-card">
       <h4 class="name"> ${this.name} </h4>
       <img class="avatar" src="${this.avatar}"/>
       <p class="health">health: <b> ${this.health} </b></p>
-      <div class="dice-container"> ${diceContent}</div>
+      <div class="dice-container"> ${this.getDiceContent()}</div>
     </div>
     `
   }
+
+  getDiceScore(){
+    return this._diceArray.reduce((score, dice)=> score + dice, 0)
+  }
+
+  takeDamage(damage){
+    console.log(`${this._name} was damage with ${damage} points`)
+  }
   
+  get enemy(){
+    return this._enemy;
+  }
+
+  set enemy(character){
+    this._enemy = character
+  }
+
   get name() {
     return this._name
   }
--- src/index.js
+++ src/index.js
@@ -1,23 +1,29 @@
 import characterData from './data.js';
 import Character from './Character.js';
 import { privateProperties, compose } from '../utils/myUtils.js';
+
+//use a map in order to create the follow relation
 const characters = [];
 const characterCards = document.getElementById('mount').children
 
 function setCharacters(){
   for (const keyCharacter in characterData) {
     characters.push(new Character(privateProperties(characterData[keyCharacter])));
-  }  
+  }
+  characters[0].enemy = characters[1];
+  characters[1].enemy = characters[0];
 }
 
-function renderCharacters(diceRolled){
+function renderCharacters(){
   //warning: its taken for granted that there is the same number of characters as of characterCards
-  characters.forEach((character, i) => characterCards[i].innerHTML = character.getInnerHtml(diceRolled))
+  characters.forEach((character, i) => characterCards[i].innerHTML = character.getInnerHtml())
 }
 
 document.body.addEventListener('click', (e) => {
   if(e.target.matches('#attack-button')){
-    renderCharacters(true);
+    characters.forEach(character => character.diceRoll());
+    characters.forEach((character) => character.takeDamage(character.enemy.getDiceScore()));
+    renderCharacters();
   }
 })
 
